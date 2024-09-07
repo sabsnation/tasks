cbecca9d681fc2bb7b8d6fc801785dd31cc27842
sab <126516676+sabsnation@users.noreply.github.com>
2024-09-06 22:58:08 -0300

    Delete tasks/AT3 directory

--- tasks/AT3/Contador_for.java
+++
@@ -1,7 +0,0 @@
-public class Contador_for {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 50; i++) {
-            System.out.println(i);
-        }
-    }
-}
--- tasks/AT3/NumerosPares.java
+++
@@ -1,9 +0,0 @@
-public class NumerosPares {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 50; i++) {
-            if (i % 2 == 0) { 
-                System.out.println(i);
-            }
-        }
-    }
-}
